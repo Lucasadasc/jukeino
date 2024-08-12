5ebbac4d076800d8f71224c95df4d50db5a6b340
Lucasadasc <71296540+Lucasadasc@users.noreply.github.com>
2024-08-12 14:40:25 -0300

    +

--- utils/esp.py
+++ utils/esp.py
@@ -4,8 +4,46 @@ import requests
 
 from time import sleep
 
-
 esp_ip = os.getenv('ESP_IP')
+
+comandos = [
+    {
+        'led' : '1',
+        'comando_desligar': 'a'
+    },
+    {
+        'led' : '2',
+        'comando_desligar': 'b'
+    },
+    {
+        'led' : '3',
+        'comando_desligar': 'c'
+    },
+    {
+        'led' : '4',
+        'comando_desligar': 'd'
+    },
+    {
+        'led' : '5',
+        'comando_desligar': 'e'
+    },
+    {
+        'led' : '6',
+        'comando_desligar': 'f'
+    },
+    {
+        'led' : '7',
+        'comando_desligar': 'g'
+    },
+    {
+        'led' : '8',
+        'comando_desligar': 'h'
+    },
+    {
+        'led' : '9',
+        'comando_desligar': 'i'
+    }
+]
 
 def gerar_sequencia(numero_piscadas: int) -> list:
     """
@@ -15,26 +53,23 @@ def gerar_sequencia(numero_piscadas: int) -> list:
     - Para cada valor da lista, liga o led correspondente (requisição POST ao ESP)
     - Retorna essa lista para o usuário
     """
-    lista_sequencia = [str(random.randint(0, 4)) for _ in range(numero_piscadas)]
+    lista_sequencia = [str(random.randint(1, 7)) for _ in range(numero_piscadas)]
     
     # Ligando e desligando leds na sequencia indicada 
     for numero in lista_sequencia:
+        # filtrando numero na lista de comandos
+        led_it = list(filter(lambda x: x['led'] == numero, comandos))
+        
+        # url para requisição
         url = f'http://{esp_ip}/echo'
         
         # ligando led
-        response = requests.post(url, numero)
-        sleep(1)
+        response = requests.post(url, led_it[0]['led'])
+        sleep(0.5)
         
-        # desligando led 
-        numero_desligar = int(numero) + 6
-        
-        if numero == 4:
-            numero_desligar = 'a' 
-        elif numero == 5:
-            numero_desligar = 'b'
-        
-        response = response = requests.post(url, str(numero_desligar))
-        sleep(1)
+        # desligando led
+        response = response = requests.post(url, str(led_it[0]['comando_desligar']))
+        sleep(0.5)
     
     return lista_sequencia
 
@@ -46,16 +81,12 @@ def ligando_led_especifico(led_id: int):
     """
     url = f'http://{esp_ip}/echo'
     
+    # filtrando led da lista de comandos 
+    led_it = list(filter(lambda x: x['led'] == str(led_id), comandos))
+    
     #Ligando led
     requests.post(url, str(led_id))
-    sleep(1)
+    sleep(0.5)
     
-    #Desligando led
-    numero_desligar = led_id + 6
-    
-    if led_id == 4:
-        numero_desligar = 'a'
-    elif led_id == 5:
-        numero_desligar = 'b'
-    
-    requests.post(url, str(numero_desligar))
+    #Desligando leds
+    requests.post(url, str(led_id[0]['comando_desligar']))
